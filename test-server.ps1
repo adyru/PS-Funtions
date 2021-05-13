fcd7b6f5ddd3407ff977f78c6b3e6376dc070fc3
adyru <42897327+adyru@users.noreply.github.com>
2021-05-13 10:39:06 +0100

    Update test-server.ps1

--- test-server.ps1
+++ test-server.ps1
@@ -19,8 +19,10 @@ Function test-server{
         )
         try
             {
+            # Check DNS and just keep the first one
             $lookups= $null
-            $Lookups = Resolve-DnsName $servertest -DnsOnly -ErrorAction Stop
+            $Lookups = (Resolve-DnsName $servertest -DnsOnly -ErrorAction Stop).IP4Address
+            $DNSCheck = $Lookups| select -First 1
             }
    
         catch [System.ComponentModel.Win32Exception] 
@@ -28,20 +30,24 @@ Function test-server{
     
         Finally
             {
+            # Null out array
             $global:CheckConnection = $null
-             If ([STRING]::IsNullOrWhitespace($Lookups.Name))
+             If ([STRING]::IsNullOrWhitespace($DNSCheck))
                 {
                 }
             Else
                 {
+                # If it is a numerical port do a check
                 if($porttest -match "^\d{1,3}")
                     {
-                    $global:CheckConnection = Test-NetConnection $servertest -port $Porttest
+                    $global:CheckConnection = Test-NetConnection $DNSCheck -port $Porttest -WarningAction SilentlyContinue
                     }
+                # If it is a well know  port do a check
                 ElseIf($porttest -eq "SMB" -or $porttest -eq "RDP" -or $porttest -eq "HTTP" -or $porttest -eq "WINRM")
                     {
-                    $global:CheckConnection = Test-NetConnection $servertest  $Porttest
+                    $global:CheckConnection = Test-NetConnection $DNSCheck    $Porttest -WarningAction SilentlyContinue
                     }
+                # if it is neither write error to screen
                 Else
                     {
                     write-host -ForegroundColor Red "You have entered and incorrect port - it needs to either be a number of one of HTTP,RDP,SMB,WINRM"
@@ -50,5 +56,3 @@ Function test-server{
             }
 
 }
-
-
