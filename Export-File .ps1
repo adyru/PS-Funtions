b9c109cfe766c38d8e9bed474e21487f8fc998de
adyru <42897327+adyru@users.noreply.github.com>
2021-05-13 16:45:00 +0100

    Update Export-File .ps1

--- Export-File .ps1
+++ Export-File .ps1
@@ -1,8 +1,19 @@
 Function Export-File {
+            <#
+        . Notes
+        =======================================
+        v1  Created on: 13/05/2021
+            Created by AU             
+        =======================================
+        . Description
+        Params are variable and filename 
+        it then exports the variable to a csv if it isnt null
+            export-file $var $varout
+        #>
      [CmdletBinding()]
         
         Param(
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory=$false)]
         [string]
         $OutVar,
 
@@ -10,10 +21,40 @@ Function Export-File {
         [string]
         $OutFile
         )
+        Begin
+            {
+            $checkFile = test-path $outfile
+            }
+        Process
+            {
+            If($checkFile -eq $true)
+                {
+                write-host -ForegroundColor Red "$($outfile) already exists"
+                return;
+                }
+            Else
+                {
+                If ([STRING]::IsNullOrWhitespace($OutVar))
+                    {
+                    write-host "Variable is null"
+                    return;
+                    }
+                Else
+                    {
+                    $OutVar | Export-Csv -NoClobber -NoTypeInformation -path $OutFile
+                    $OutcheckFile = test-path $outfile
+                    If($OutcheckFile -eq $true)
+                        {write-host -ForegroundColor Green "Variable exported to $($OutFile)"}
+                    Else
+                        {write-host -ForegroundColor red "Variable not exported to $($OutFile)"}
+                    }
+                }
+            
+ 
 
-        If ([STRING]::IsNullOrWhitespace($OutVar))
-            {write-host "$($OutVar) is null"}
-        Else
-            {$OutVar | Export-Csv -NoClobber -NoTypeInformation -path $OutFile}
+    }
+        End
+           {
 
+           }
 }
