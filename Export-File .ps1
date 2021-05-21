808000e8ce5dc498991e5f00a36f78f5c78bc0e0
adyru <42897327+adyru@users.noreply.github.com>
2021-05-21 13:54:04 +0100

    Update Export-File .ps1

--- Export-File .ps1
+++ Export-File .ps1
@@ -3,11 +3,13 @@ Function Export-File {
         . Notes
         =======================================
         v1  Created on: 13/05/2021
-            Created by AU             
+            Created by AU  
+        V2 - 21/05/2021 Added in Check so that if files exists it exports with a random digit inserted           
         =======================================
         . Description
         Params are variable and filename 
         it then exports the variable to a csv if it isnt null
+        If files exists adds in some random digits to makes sure there is an export
             export-file $var $varout
         #>
      [CmdletBinding()]
@@ -30,12 +32,25 @@ Function Export-File {
             {
             If($checkFile -eq $true)
                 {
+                # if the files exists we will use  a random number into the filename 
                 write-host -ForegroundColor Red "$($outfile) already exists"
-                $Random = Get-Random -Minimum 1 -Maximum 100
-                $newOut =  $outfile -replace ".csv","$($Random).csv"
-                $newout
-                $OutVar | Export-Csv -NoClobber -NoTypeInformation -path $newOut
-                write-host -ForegroundColor Green "Variable exported to $($newOut)"
+
+                If (!($OutVar))
+                    {
+                    write-host "Variable is null"
+                    return;
+                    }
+                Else
+                    {
+                    $Random = Get-Random -Minimum 1 -Maximum 100
+                    $newOut =  $outfile -replace ".csv","$($Random).csv"
+                    $OutVar | Export-Csv -NoClobber -NoTypeInformation -path $newout
+                    $OutcheckFile = test-path $outfile
+                    If($OutcheckFile -eq $true)
+                        {write-host -ForegroundColor Green "Variable exported to $($newout)"}
+                    Else
+                        {write-host -ForegroundColor red "Variable not exported to $($newout)"}
+                    }
                 return;
                 }
             Else
@@ -52,9 +67,7 @@ Function Export-File {
                     If($OutcheckFile -eq $true)
                         {write-host -ForegroundColor Green "Variable exported to $($OutFile)"}
                     Else
-                        {
-                        write-host -ForegroundColor Green "Variable exported to $($newOut)"
-                        }
+                        {write-host -ForegroundColor red "Variable not exported to $($OutFile)"}
                     }
                 }
             
@@ -67,7 +80,6 @@ Function Export-File {
            }
 }
 
-$a = "test\"
-$out = "out.csv"
-
-Export-File $a $out
+$a = "a"
+$aout = "filewe3wewqeq.csv"
+Export-File $a $aout
